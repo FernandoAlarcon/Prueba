1cfa7d32532bed797a37c0b6739bf24adcd0b796
FernandoAlarcon <erickfernando_20@hotmail.com>
2020-07-17 07:17:06 -0500

    cimmit1

---
+++ resources/views/info.blade.php
@@ -0,0 +1,5 @@
+
+<textarea class="form-control" placeholder="Write Here" v-model="text" ></textarea>
+<button type="button" class="btn btn-warning" v-on:click.prevent="LoadData()" >
+	Enviar
+</button>
